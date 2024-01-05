6660e70388bd20ec35dcd1a86f1a01f5173af660
Daniel Seiler <73987329+daniel-seiler@users.noreply.github.com>
2024-01-05 21:18:59 +0100

    fixed iso layout and ms word launch button

--- keyboards/coseyfannitutti/discipline/keymaps/windows/keymap.c
+++ keyboards/coseyfannitutti/discipline/keymaps/windows/keymap.c
@@ -24,12 +24,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case MS_EXCEL:
             if (record->event.pressed) {
-                SEND_STRING(SS_LGUI("r") SS_DELAY(10) "excel" SS_TAP(X_ENT));
+                SEND_STRING(SS_LGUI("r") SS_DELAY(100) "excel" SS_TAP(X_ENT));
             }
             break;
         case MS_WORD:
             if (record->event.pressed) {
-                SEND_STRING(SS_LGUI("r") SS_DELAY(10) "word" SS_TAP(X_ENT));
+                SEND_STRING(SS_LGUI("r") SS_DELAY(100) "WinWord.exe" SS_TAP(X_ENT));
             }
             break;
     }
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV, KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,  KC_DEL,
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC,           KC_PGUP,
       KC_ESC, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NUHS, KC_ENT,   KC_PSCR,
-      KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_MINS,  KC_RSFT, KC_UP,    KC_PGDN,
+      KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,    KC_PGDN,
       KC_LCTL, KC_LGUI, KC_LALT,                          KC_SPC,                 KC_RALT, MO(1), KC_F8,  KC_LEFT, KC_DOWN,  KC_RIGHT),
 
   [1] = LAYOUT_65_iso(
